f4dbcc19dfeb69ef97a88c0bdf5b32737fd934e9
wburningham <wburningham@gmail.com>
2012-04-29 11:43:46 -0600

    Update custom.aliases.bash

--- custom.aliases.bash
+++ custom.aliases.bash
@@ -1,4 +1,4 @@
-source /usr/local/bin/virtualenvwrapper.sh
+#source /usr/local/bin/virtualenvwrapper.sh
 PATH=$PATH:/usr/local/mysql/bin
 export DYLD_LIBRARY_PATH=/usr/local/mysql/lib/
 
@@ -10,10 +10,12 @@ export DYLD_LIBRARY_PATH=/usr/local/mysql/lib/
 function newdir() { mkdir -p "$@" && eval cd "\"\$$#\""; }
 function cl { cd $1; ls; }
 # alias home='cd ~'
-alias desk='cd ~/Desktop'
 alias list='ls -aFhlG'
 function .. { cd ..; ls; }
 alias c='clear'
+alias d="cd ~/Dropbox"
+alias p="cd ~/Projects"
+alias desk='cd ~/Desktop'
 
 #-------------------------------------------------------------------
 #     Networking Functions and Aliases
@@ -61,12 +63,10 @@ ip; echo ""; mac;
 #-------------------------------------------------------------------
 
 alias profile="vim ~/.bash_profile"
-alias sshmylink='ssh webadmin@mylink.byu.edu'
-alias sshmylinkdb='ssh dbadmin@10.18.12.5'
-alias sshcapstone='ssh root@192.168.246.5'
-alias sshnursing='ssh -p 44625 root@it.et.byu.edu'
 alias www='cd /Library/WebServer/Documents'
 alias msg="vim Dropbox/Shared\ Folders/RAW/.msg"
+
+
+
 # http://bit.ly/igCErk
 alias m='mvim --remote-silent'
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
